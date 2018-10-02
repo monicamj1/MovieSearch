10b5e007657a0e37ff6bce195c2b5df8dc7dbb58
Mónica Moreno <mmoreno856@gmail.com>
2018-10-02 08:33:42 +0200

    Versión inicial de MovieSearch: only layout.

---
+++ app/src/main/java/com/example/monicamj1/moviesearch/MovieActivity.java
@@ -0,0 +1,13 @@
+package com.example.monicamj1.moviesearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
